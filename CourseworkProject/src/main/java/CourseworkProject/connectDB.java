692e57e3752dd9fa9226fe91e90fc1c30d71fe2e
lucas <lucas@DESKTOP-69327SR>
2022-04-16 14:58:34 +0100

    Fixed the password without encryption being registered in the DB

--- CourseworkProject/src/main/java/CourseworkProject/connectDB.java
+++ CourseworkProject/src/main/java/CourseworkProject/connectDB.java
@@ -15,14 +15,30 @@ import javax.swing.JOptionPane;
  */
 public class connectDB {
     
-      public static Connection getConnection() {
-
+    String hostName = "sql4.freesqldatabase.com";
+    String databasePort = "3306";
+    String databaseName = "sql4484735";
+    String userName = "sql4484735";
+    String password = "MKxsG3f8yK";
+    
+      public Connection getConnection() {
+//        Connection con = null;
+//        Properties connectionProps = new Properties();
+//        connectionProps.put("userName", this.userName);
+//        connectionProps.put("password", this.password);
         try {
-            
             Class.forName("com.mysql.cj.jdbc.Driver");
 //            String url = "jdbc:mysql://phpmyadmin.ecs.westminster.ac.uk:3306/w1783737_0?zeroDateTimeBehavior=convertToNull";
-            String url = "jdbc:mysql://sql4.freesqldatabase.com:3306/sql4483459?zeroDateTimeBehavior=convertToNull";
-            Connection con = DriverManager.getConnection(url);
+//            String url = "jdbc:mysql://sql4.freesqldatabase.com:3306/sql4483459?zeroDateTimeBehavior=convertToNull";
+
+            String url = "jdbc:mysql://"+this.hostName+":"+this.databasePort+"/"+this.databaseName+"?zeroDateTimeBehavior=convertToNull";
+            Connection con = DriverManager.getConnection(url, userName, password);
+//
+//        conn = DriverManager.getConnection(
+//                   "jdbc:" + this.dbms + "://" +
+//                   this.serverName +
+//                   ":" + this.portNumber + "/",
+//                   connectionProps);
 
             JOptionPane.showMessageDialog(null, "Connection Established");
             return con;
